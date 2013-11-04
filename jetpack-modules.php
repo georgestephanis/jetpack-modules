ed2f38ccb98e2503fe8f2de83da2a7c59ec01183
George Stephanis <george@stephanis.info>
2013-11-04 10:53:50 -0400

    Make sure we don't load it if Jetpack isn't active.

--- jetpack-modules.php
+++ jetpack-modules.php
@@ -9,6 +9,9 @@
  * Author URI: http://stephanis.info/
  */
 
-if ( is_admin() ) {
-	require_once( dirname(__FILE__) . '/class.jetpack-modules.php' );
+add_action( 'init', 'load_jetpack_modules_class' );
+function load_jetpack_modules_class() {
+    if ( is_admin() && class_exists( 'Jetpack' ) ) {
+        require_once( dirname(__FILE__) . '/class.jetpack-modules.php' );
+    }
 }
