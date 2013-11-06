11262475fec8e549f1439ff9b75b3bcc1913dc79
George Stephanis <george@stephanis.info>
2013-11-06 14:06:02 -0400

    Stop == comparing to true, let it stand on its own.

--- js/models.jetpack-modules.js
+++ js/models.jetpack-modules.js
@@ -47,7 +47,7 @@ window.jetpackModules.models = (function( window, $, _, Backbone ) {
 			 */
 			deactivate_module : function( module_slug ) {
 				var modules = _.clone( this.get( 'raw' ) );
-				if ( modules[ module_slug ] && true == modules[ module_slug ].activated ) {
+				if ( modules[ module_slug ] && modules[ module_slug ].activated ) {
 					modules[ module_slug ].activated = false;
 
 					/* @todo: Update server via ajaxy goodness here. */
